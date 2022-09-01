b25624a945b10257556ea7233de85d07485eaa1e
taina k lima <tainaklima.0912@gmail.com>
2022-08-31 21:47:23 -0300

    instalando router dom e fazendo o primeiro ajuste

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -11,9 +16,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
